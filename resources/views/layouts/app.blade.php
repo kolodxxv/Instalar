f374b38a6ca2e3acfb7252768f33355f48474f6e
kolodxxv <crowcrxst@gmail.com>
2024-01-19 12:02:50 +0200

    IL-Sidenav - Fixed logout position and logo color change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Instalar') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -20,10 +20,11 @@
 </head>
 <body>
     <div id="app">
-        <Navbar></Navbar>
         <main class="py-4">
+            <Navbar></Navbar>
             @yield('content')
         </main>
+       
     </div>
 </body>
 </html>
